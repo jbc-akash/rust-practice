ee1decf3a478ba429823bd2c24f2c5f7dab57967
mostafa akash <jbc.mostafa@gmail.com>
2025-06-25 16:06:41 +0600

    Add comments for additional core concepts: outline areas for future implementation

--- src/core_concepts/mod.rs
+++ src/core_concepts/mod.rs
@@ -3,3 +3,13 @@ pub mod collections;
 pub mod ownership;
 pub mod traits;
 pub mod structs_enums;
+// Generic
+// Error Handling
+// Concurrency
+// Closure/Iterators
+// Macros
+// Unsafe rust
+// Smart Pointers
+// Pattern Matching patterns
+// Iterators in depth
+// File i/o
